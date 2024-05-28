77730dfeb16a7b808f130f568482de61ceae6857
futfetish <gr1zzly.medvegonok@gmail.com>
2024-05-28 15:13:11 +0300

    webpack config ts

---
+++ webpack.config.ts
@@ -0,0 +1,35 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import "webpack-dev-server";
+
+module.exports = (env: any) => {
+  const mode = env.mode ?? "development";
+  const config: webpack.Configuration = {
+    mode: mode,
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    devServer: {
+      static: "./dist",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "document.html"),
+      }),
+    ],
+  };
+  return config;
+};
